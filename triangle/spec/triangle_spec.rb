0133e1e56b05fe78b47777bee83e91c49d2b1c6d
Natasha Waller <natashawaller@Natashas-MacBook-Pro-2.local>
2019-09-19 14:35:37 -0400

    Add test for creation of triangle object. Create Triangle class.

--- triangle/spec/triangle_spec.rb
+++ triangle/spec/triangle_spec.rb
@@ -1,2 +1,10 @@
 require_relative './spec_helper.rb'
 require_relative '../triangle.rb'
+
+RSpec.describe Triangle do
+    it "creates a triangle object" do
+        triangle = Triangle.new
+        expect(triangle).to be_an_instance_of(Triangle)
+    end
+end
+        
