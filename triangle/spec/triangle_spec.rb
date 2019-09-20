bafffd891f6d506f3d3536fb828b45f04cdaa621
Natasha Waller <natashawaller@Natashas-MacBook-Pro-2.local>
2019-09-19 20:07:49 -0400

    Add test to triangle_spec.rb to check that triangle objects are initialized with 3 arguments within the Triangle class in triangle.rb.

--- triangle/spec/triangle_spec.rb
+++ triangle/spec/triangle_spec.rb
@@ -1,10 +1,12 @@
 require_relative './spec_helper.rb'
 require_relative '../triangle.rb'
 
-RSpec.describe Triangle do
-    it "creates a triangle object" do
-        triangle = Triangle.new
-        expect(triangle).to be_an_instance_of(Triangle)
+describe Triangle do
+  describe "initialize method" do
+    it "initializes a triangle object with 3 arguments" do
+        triangle_1 = Triangle.new(3,4,7)
+        expect(triangle_1).to be_an_instance_of(Triangle)
     end
+  end
 end
-        
+
