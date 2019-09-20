1542ce6268cf30076db7e6e3d29dc35f8b3f295b
Natasha Waller <natashawaller@Natashas-MacBook-Pro-2.local>
2019-09-20 13:14:30 -0400

    Add test to check for valid triangles. Add TriangleError class with custom message to triangle.rb.  Adjust valid_triangle method to verify valid triangles and raise custom error if invalid.

--- triangle/spec/triangle_spec.rb
+++ triangle/spec/triangle_spec.rb
@@ -30,3 +30,22 @@ describe "side_3 method" do
         expect(((triangle_1.side_3).is_a?(Integer)) && (triangle_1.side_3 > 0)).to be_truthy
     end
 end
+
+describe "valid_triangle method" do
+    it "raises TriangleError if the triangle object is invalid" do
+         triangle_2 = Triangle.new(2,2,7)
+         begin
+           expect(triangle_2.valid_triangle).to raise_error(TriangleError)
+           rescue TriangleError => error
+           error.message
+         end   
+    end
+
+    it "returns the correct type of triangle if the triangle is valid" do
+        triangle_1 = Triangle.new(3,4,7)
+        expect(triangle_1.valid_triangle).to eq("scalene")
+    end
+end
+
+
+
--- triangle/triangle.rb
+++ triangle/triangle.rb
@@ -6,5 +6,31 @@ class Triangle
         @side_2 = side_2
         @side_3 = side_3
     end
+
+    def valid_triangle
+        
+        if !((side_1 + side_2) >= side_3 && (side_2 + side_3) >= side_1 && (side_3 + side_1) >= side_2)
+             raise TriangleError
+        
+        elsif side_1 == side_2 && side_1 == side_3
+            "equilateral"
+
+        elsif 
+            side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+            "isosceles"
+
+        elsif 
+            side_1 != side_2 && side_2 != side_3 && side_3 != side_1
+            "scalene"
+        end
+    end
     
+end 
+
+class TriangleError < StandardError
+
+    def message
+        "Triangle cannot exist!  The sum of the lengths of any two sides of a triangle always equals or exceeds the length of the third side."
+    end
+
 end 
