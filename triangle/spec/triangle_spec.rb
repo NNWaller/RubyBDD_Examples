8ca419eb3107065e9a39a343842075ebec9612c0
Natasha Waller <natashawaller@Natashas-MacBook-Pro-2.local>
2019-09-26 14:45:02 -0400

    Refactor triangle_spec.rb. Add test and code for 0 value arguments.

--- triangle/spec/triangle_spec.rb
+++ triangle/spec/triangle_spec.rb
@@ -3,36 +3,28 @@ require_relative '../triangle.rb'
 
 describe Triangle do
   describe "initialize method" do
+    let(:triangle_1) {Triangle.new(3,4,7)}
+
     it "initializes a triangle object with 3 arguments" do
-        triangle_1 = Triangle.new(3,4,7)
-        expect(triangle_1).to be_an_instance_of(Triangle)
+      expect(triangle_1).to be_an_instance_of(Triangle)
+    end
+
+    it "has a value for side_1" do
+      expect(triangle_1.side_1).to eq(3)
+    end
+   
+    it "has a value for side_2" do
+      expect(triangle_1.side_2).to eq(4)
+    end
+
+    it "has a value for side_3" do
+        expect(triangle_1.side_3).to eq(7)
     end
   end
 end
 
-describe "side_1 method" do 
-    it "returns an integer > 0" do
-        triangle_1 = Triangle.new(3,4,7)
-        expect(((triangle_1.side_1).is_a?(Integer)) && (triangle_1.side_1 > 0)).to be_truthy
-    end
-end
-
-describe "side_2 method" do 
-    it "returns an integer > 0" do
-        triangle_1 = Triangle.new(3,4,7)
-        expect(((triangle_1.side_2).is_a?(Integer)) && (triangle_1.side_2 > 0)).to be_truthy
-    end
-end
-
-describe "side_3 method" do 
-    it "returns an integer > 0" do
-        triangle_1 = Triangle.new(3,4,7)
-        expect(((triangle_1.side_3).is_a?(Integer)) && (triangle_1.side_3 > 0)).to be_truthy
-    end
-end
-
 describe "valid_triangle method" do
-    it "raises TriangleError if the triangle object is invalid" do
+    it "raises TriangleError if the triangle object violates triangle inequality principle" do
          triangle_2 = Triangle.new(2,2,7)
          begin
            expect(triangle_2.valid_triangle).to raise_error(TriangleError)
@@ -41,11 +33,17 @@ describe "valid_triangle method" do
          end   
     end
 
+    it "raises TriangleError if any side value is less than or equal to zero" do
+        triangle_3 = Triangle.new(0,0,0)
+        begin
+          expect(triangle_3.valid_triangle).to raise_error(TriangleError)
+          rescue TriangleError => error
+          error.message
+        end   
+   end
+
     it "returns the correct type of triangle if the triangle is valid" do
         triangle_1 = Triangle.new(3,4,7)
         expect(triangle_1.valid_triangle).to eq("scalene")
     end
 end
-
-
-
--- triangle/triangle.rb
+++ triangle/triangle.rb
@@ -11,6 +11,9 @@ class Triangle
         
         if !((side_1 + side_2) >= side_3 && (side_2 + side_3) >= side_1 && (side_3 + side_1) >= side_2)
              raise TriangleError
+
+        elsif side_1 <= 0 || side_2 <= 0 || side_3 <= 0 
+             raise TriangleError
         
         elsif side_1 == side_2 && side_1 == side_3
             "equilateral"
@@ -33,4 +36,4 @@ class TriangleError < StandardError
         "Triangle cannot exist!  The sum of the lengths of any two sides of a triangle always equals or exceeds the length of the third side."
     end
 
-end 
+end                                                                              
